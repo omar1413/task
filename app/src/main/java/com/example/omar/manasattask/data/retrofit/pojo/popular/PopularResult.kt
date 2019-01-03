4150a121c7d5e447fc1d97a68a9197d9772e2356
omar1413 <omarosama200100@gmail.com>
2019-01-03 09:14:13 +0200

    refactor layout of popular persons

--- app/src/main/java/com/example/omar/manasattask/data/retrofit/pojo/popular/PopularResult.kt
+++ app/src/main/java/com/example/omar/manasattask/data/retrofit/pojo/popular/PopularResult.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class PopularResult(
     val id: Int,
     val name: String,
-    val popularity: Float,
+    val popularity: Double,
     @SerializedName("profile_path")val profilePath:String
 )
--- app/src/main/java/com/example/omar/manasattask/ui/main/PopularPersonsPresenter.kt
+++ app/src/main/java/com/example/omar/manasattask/ui/main/PopularPersonsPresenter.kt
@@ -1,37 +1,39 @@
 package com.example.omar.manasattask.ui.main
 
+import com.example.omar.manasattask.BuildConfig
 import com.example.omar.manasattask.data.MvpModel
-import com.example.omar.manasattask.data.retrofit.pojo.popular.PopularResult
 import com.example.omar.manasattask.models.PopularPerson
 import com.example.omar.manasattask.ui.base.BasePresenter
 import io.reactivex.Observable
-import io.reactivex.Single
-import io.reactivex.SingleObserver
-import io.reactivex.SingleSource
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import java.util.*
 import javax.inject.Inject
 
-class PopularPersonsPresenter<V : PopularPersonsMvpView> @Inject constructor(val dataManager: MvpModel) : BasePresenter<V>(),
+class PopularPersonsPresenter<V : PopularPersonsMvpView> @Inject constructor(val dataManager: MvpModel) :
+    BasePresenter<V>(),
     PopularPersonsMvpPresenter<V> {
 
 
     override fun listPopularPersons() {
 
         val disposable = dataManager.getPopularPeopleByPage(1)
-//            .map {
-//                it.results
-//            }
-//            .flatMapObservable {
-//                Observable.fromIterable(it)
-//            }
             .subscribeOn(Schedulers.io())
+            .map {
+                it.results
+            }
             .toObservable()
+            .flatMap {
+                Observable.fromIterable(it)
+            }
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                it ->
-                //mvpView?.addPopularPersonToTheList(PopularPerson(it.id,it.name,it.popularity,it.profilePath))
+            .subscribe { it ->
+                mvpView?.addPopularPersonToTheList(
+                    PopularPerson(
+                        it.id, it.name,
+                        it.popularity,
+                        BuildConfig.Base_IMAGE_URL + it.profilePath
+                    )
+                )
             }
 
     }
